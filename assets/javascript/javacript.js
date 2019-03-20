39e138ea84d4d6fe825ed3cc46f7e93866e11f5d
unknown <chugm1989@gmail.com>
2019-03-20 18:31:06 -0400

    working_api_call

--- assets/javascript/javacript.js
+++ assets/javascript/javacript.js
@@ -1,1 +1,25 @@
-d
+// GLOBAL VARIABLES; 21 TO HOLD API CALLS
+/* var apiURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=U6Zjyt5eoaIP28XWjbXBq7mRgSYfEBlZ&limit=5";
+console.log(apiURL); */
+
+// BUTTON CLICK THAT STORES <INPUT> VALUE INTO OBJECT, ADDS OBJECT TO API CALL, RETURNS JSON
+$(".btn").click(function(){
+ //   alert("Yep!");
+ var userInput = $(".form-control").val().trim();
+console.log(userInput);
+var apiURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=U6Zjyt5eoaIP28XWjbXBq7mRgSYfEBlZ&limit=5";
+console.log(apiURL);
+
+});
+
+// AJAX API
+/* $.ajax({
+    url: apiURL,
+    method: "GET",
+}).then(function(response) {
+
+}); */
+
+// STORE VALUE OF INPUT FIELD: THIS WORKS 03/20/2019
+/* var userInput = $(".form-control").val().trim();
+console.log(userInput); */
