e3933d47d7ea60ccc4109bd941f6032b3df58d19
unknown <chugm1989@gmail.com>
2019-03-23 21:49:39 -0400

    hw_submission_updated

--- assets/javascript/javacript.js
+++ assets/javascript/javacript.js
@@ -3,36 +3,59 @@
 console.log(apiURL); */
 var topics = ["Rick+and+Morty", "Bobs+Burgers", "Its+Always+Sunny+in+Philadelphia", "That+70s+Show", "Workaholics", "Key+Peele", "Atlanta"];
 
-// BUTTON CLICK THAT STORES <INPUT> VALUE INTO OBJECT, ADDS OBJECT TO API CALL, RETURNS JSON, APPENDS JSON TO WEBPAGE: THIS WORKS 03/20/2019
-$("body").on("click", "button.btn", function (response){
- for (i = 0; i <topics.length; i++) {
- console.log(topics[i]);
- 
-// var apiURL = "http://api.giphy.com/v1/gifs/search?q=" + userInput + "&api_key=U6Zjyt5eoaIP28XWjbXBq7mRgSYfEBlZ&limit=5";
-// var apiURL = `https://api.giphy.com/v1/gifs/search?api_key=U6Zjyt5eoaIP28XWjbXBq7mRgSYfEBlZ&q=${userInput}&limit=10&offset=0&rating=G&lang=en`;
-var apiURL = `https://api.giphy.com/v1/gifs/search?api_key=U6Zjyt5eoaIP28XWjbXBq7mRgSYfEBlZ&q= ${topics[i]} &limit=10&offset=0&rating=G&lang=en`;
-console.log(apiURL);
- }
-$.ajax({
-    url: apiURL,
-    method: "GET",
-}).then(function(response) {
-    $("#buttonHolder").empty();
-    $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[0]} `);
-    $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[1]} `);
-    $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[2]} `);
-    $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[3]} `)
-    $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[4]} `)
-    $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[5]} `)
-    $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[6]} `)
 
- //  alert("weeee!");
-   $("#displayGif").prepend(`
-    <div class="showGif"> <img src="${response.data[0].images.original_still.url}"> </div>); 
-    <div class="showGif"> <img src="${response.data[1].images.original_still.url}"> </div>); 
-    <div class="showGif"> <img src="${response.data[2].images.original_still.url}"> </div>`); 
-    console.log(response[i]); 
+// BUTTON CLICK THAT GENERATES DYNAMIC BUTTONS: THIS WORKS 03/23/2019
+ $("body").on("click", "button", function() {
+    $("#buttonHolder").empty();
+    for (i = 0; i <topics.length; i++) {
+        console.log(topics[i]);
+    var t = $("<button>")
+    t.addClass("button");
+    t.attr("button-click");
+    t.text(topics[i]);
+    $("#buttonHolder").append(t);
+ }
 })
+$("button").on("click", function(){
+    for (i = 0; i < topics.length; i++) {
+    var apiURL = `https://api.giphy.com/v1/gifs/search?api_key=U6Zjyt5eoaIP28XWjbXBq7mRgSYfEBlZ&q= ${topics[i]} &limit=10&offset=0&rating=G&lang=en`;
+    $.ajax({
+        url: apiURL,
+        method: "GET",
+    }).then(function(apiCall) {
+     //   var gifHolder = $("<div> <img> </div>")
+     //   var gifMaker = apiCall.data[0].bitly_gif_url;
+      //  gifHolder.attr(apiCall.data[0].bitly_gif_ur);
+    //    $("#displayGif").append("src", apiCall.data[0].bitly_gif_ur);
+        console.log(apiCall.data[0].images.downsized_medium.url);
+        $("#displayGif").prepend(` <div class="showGif"> <img src="${apiCall.data[0].images.downsized_medium.url}"> </div> `); 
+        
+    })
+}
+})
+// $.ajax({
+//     url: apiURL,
+//     method: "GET",
+// }).then(function(response) {
+//     for (i = 0; i <topics.length; i++) {
+//         console.log(topics[i]); 
+//     $("#buttonHolder").empty();
+
+//     }
+//     });
+    // $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[1]} `);
+    // $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[2]} `);
+    // $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[3]} `)
+    // $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[4]} `)
+    // $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[5]} `)
+    // $("#buttonHolder").append(` <button class="btn bg-warning" type="button"> ${topics[6]} `)
+
+//    $("#displayGif").prepend(`
+//     <div class="showGif"> <img src="${response.data[0].images.original_still.url}"> </div>); 
+//     <div class="showGif"> <img src="${response.data[1].images.original_still.url}"> </div>); 
+//     <div class="showGif"> <img src="${response.data[2].images.original_still.url}"> </div>`); 
+//     console.log(response[i]); 
+//     });
 
 /* $(".gifButton").on("click", function() {
     event.preventDefault();
@@ -40,7 +63,7 @@ $.ajax({
     gifButton(grabGif);
     $("#displayGif").prepend("Hi") */
 
-});
+
 
 /* function makeButton() {
     $(".form-inline").empty();
